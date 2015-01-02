d0403990d596ed74818562e085397cbbb55a74b3
Anthony Alaribe <anthonyalaribe@ymail.com>
2015-01-02 15:15:25 +0000

    added angular js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,50 +1,60 @@
-/*!
- *
- *  Web Starter Kit
- *  Copyright 2014 Google Inc. All rights reserved.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *    https://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License
- *
- */
-(function () {
-  'use strict';
+'use strict';
+var app = angular.module('app', ['ui.bootstrap', 'facebook','autocomplete']);
 
-  var querySelector = document.querySelector.bind(document);
-
-  var navdrawerContainer = querySelector('.navdrawer-container');
-  var body = document.body;
-  var appbarElement = querySelector('.app-bar');
-  var menuBtn = querySelector('.menu');
-  var main = querySelector('main');
-
-  function closeMenu() {
-    body.classList.remove('open');
-    appbarElement.classList.remove('open');
-    navdrawerContainer.classList.remove('open');
-  }
-
-  function toggleMenu() {
-    body.classList.toggle('open');
-    appbarElement.classList.toggle('open');
-    navdrawerContainer.classList.toggle('open');
-    navdrawerContainer.classList.add('opened');
-  }
-
-  main.addEventListener('click', closeMenu);
-  menuBtn.addEventListener('click', toggleMenu);
-  navdrawerContainer.addEventListener('click', function (event) {
-    if (event.target.nodeName === 'A' || event.target.nodeName === 'LI') {
-      closeMenu();
-    }
-  });
-})();
+app.config(function(FacebookProvider) {
+     // Set your appId through the setAppId method or
+     // use the shortcut in the initialize method directly.
+     FacebookProvider.init('747991285264118');
+  })
+  
+  
+.controller('LoginCtrl',  function($scope, $modal, $http, Facebook){
+  $scope.user = {};
+  $scope.pic = {};
+  $scope.loggedIn =false;
+  
+  $scope.logout = function(){
+    Facebook.logout(function(response) {
+  // user is now logged out
+      $scope.loggedIn = false;
+    });
+  };
+  
+    $scope.login = function() {
+        // From now on you can use the Facebook service just as Facebook api says
+        Facebook.login(function(response) {
+          // Do something with response
+          $http.post('/login', response);
+            
+            
+         });
+      };
+  
+  
+  $scope.$watch(function() {
+    // This is for convenience, to notify if Facebook is loaded and ready to go.
+    return Facebook.isReady();
+  }, function(newVal) {
+    // You might want to use this to disable/show/hide buttons and else
+    $scope.facebookReady = true;
+     Facebook.getLoginStatus(function(response) {
+            if(response.status === 'connected') {
+              if ($scope.loggedIn !== true){
+                $scope.loggedIn = true;
+                Facebook.api('/me', function(response) {
+                    $scope.user = response;
+                    $http.post('/login', response);
+                    console.log(response);
+                });
+                Facebook.api('/me/picture?height=100&type=square&width=100', function(response) {
+                    $scope.pic= response;
+                });
+                
+              }
+            } else {
+              $scope.loggedIn = false;
+            }
+          });
+      });
+});
+  
